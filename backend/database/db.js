121dd51b3d4f11bdf3ff33145329452e3155ba2c
rashi arora <74211043+rashi12122001@users.noreply.github.com>
2024-01-17 20:39:00 +0530

    db updated

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,13 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const Connection = async (username, password) => {
-    const URL = `mongodb+srv://${username}:${password}@ac-sx690z8-shard-00-00.3mh5ouf.mongodb.net:27017,ac-sx690z8-shard-00-01.3mh5ouf.mongodb.net:27017,ac-sx690z8-shard-00-02.3mh5ouf.mongodb.net:27017/?ssl=true&replicaSet=atlas-gecv59-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    try {
-        await mongoose.connect(URL, { useNewUrlParser: false, useUnifiedTopology: true }); // Async
-        console.log('Database connected successfully');
-    } catch (error) {
-        console.log('Error', error);
-    }
+  const URL =
+    "mongodb+srv://shreyashchandra123:ReDJPvY7RCh1Nahd@shreyash.3mh5ouf.mongodb.net/secret-blog";
+  try {
+    await mongoose.connect(URL, {
+      useNewUrlParser: false,
+      useUnifiedTopology: true,
+    }); // Async
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Error", error);
+  }
 };
 
 export default Connection;
+
